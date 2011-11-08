68e549c4f77cc1f6f3b06513d576b11a84f11b04
kwebster <keith.webster@terracor.ca>
2011-11-08 13:21:24 -0600

    Initial commit

---
+++ src/microblog_api/core.clj
@@ -0,0 +1,1 @@
+(ns microblog-api.core)
---
+++ test/microblog_api/test/core.clj
@@ -0,0 +1,6 @@
+(ns microblog-api.test.core
+  (:use [microblog-api.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
