475223bb27a26f6dff076fb1712051fcdeb73d8d
Marc Jeanson <marc@redlinesoftware.com>
2011-11-08 14:59:10 -0600

    implemented save-post

--- src/microblog_api/core.clj
+++ src/microblog_api/core.clj
@@ -1,13 +1,19 @@
 (ns microblog-api.core)
+;(use 'microblog-api.core :reload-all)
 
 (def last-id (atom 0))
+
+(def storage (atom {}))
 
 (defn gen-post-id []
   (swap! last-id inc))
 
 (defn save-post [post]
+  (swap! storage (fn [storage]
+                   (update-in storage [:id-index] assoc (:id post) post)
+                   ))
   post
-)
+  )
 
 (defn create-post [user text]
   (let [post {:post-id (gen-post-id)  :current-time (System/currentTimeMillis) :user user :text text}]
@@ -19,3 +25,4 @@
 
 (defn timeline [offset n]
 )
+
