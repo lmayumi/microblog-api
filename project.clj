133038aaeda3032260b162d39b4f4ebd9ff6e334
Marc Jeanson <marc@redlinesoftware.com>
2011-11-08 13:29:21 -0600

    adding ring

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject microblog-api "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-jetty-adapter "1.0.0-RC1"]
+                 [ring "1.0.0-RC1"] 
+                 ])
