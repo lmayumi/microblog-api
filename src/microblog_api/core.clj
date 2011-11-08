ec9a1f6051cc07a41e5084a7b0da1bb671b7bfc7
Mayumi Liyanage <mayumi.liyanage@gmail.com>
2011-11-08 15:03:30 -0600

    Added updating storage

--- src/microblog_api/core.clj
+++ src/microblog_api/core.clj
@@ -1,16 +1,22 @@
 (ns microblog-api.core)
+;(use 'microblog-api.core :reload-all)
 
 (def last-id (atom 0))
+(def storage (atom {}))
 
 (defn gen-post-id []
   (swap! last-id inc))
 
 (defn save-post [post]
+    (swap! storage (fn [storage]
+                     (update-in storage [:id-index] assoc (:id post) post)
+                     ))
   post
 )
+    
 
 (defn create-post [user text]
-  (let [post {:post-id (gen-post-id)  :current-time (System/currentTimeMillis) :user user :text text}]
+  (let [post {:id (gen-post-id)  :current-time (System/currentTimeMillis) :user user :text text}]
     (save-post post))
 )
 
