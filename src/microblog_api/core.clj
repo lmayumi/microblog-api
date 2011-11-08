70cbd8bf56610576e536c8965be4eaee686188bd
kwebster <keith.webster@terracor.ca>
2011-11-08 14:12:59 -0600

    Added skeleton functions and gen implementation

--- src/microblog_api/core.clj
+++ src/microblog_api/core.clj
@@ -1,1 +1,21 @@
 (ns microblog-api.core)
+
+(def last-id (atom 0))
+
+(defn gen-post-id []
+  (swap! last-id inc))
+
+(defn save-post [post]
+  post
+)
+
+(defn create-post [user text]
+  (let [post {:post-id (gen-post-id)  :current-time (System/currentTimeMillis) :user user :text text}]
+    (save-post post))
+)
+
+(defn get-post [post-id]
+)
+
+(defn timeline [offset n]
+)
